9eb74698bb8ec01369180b3ef4bb524a72a2ece8
44670 <44670@users.noreply.github.com>
2016-09-19 19:49:37 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NTRViewer
 PC Viewer for NTR CFW's streaming feature
+
+# Dependencies
+SDL2: https://www.libsdl.org/download-2.0.php
+
+libjpeg-turbo: http://libjpeg-turbo.virtualgl.org/
+
+# License
+GPLv2
---
+++ stdafx.h
@@ -0,0 +1,111 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+#include <iostream>
+
+#include <winsock2.h>  
+
+#include <stdint.h>
+
+//! 8 bit unsigned integer.
+typedef uint8_t		uint8;
+//! 16 bit unsigned integer.
+typedef uint16_t	uint16;
+//! 32 bit unsigned integer.
+typedef uint32_t	uint32;
+//! 64 bit unsigned integer.
+typedef uint64_t	uint64;
+
+//! 8 bit signed integer.
+typedef int8_t		int8;
+//! 16 bit signed integer.
+typedef int16_t		int16;
+//! 32 bit signed integer.
+typedef int32_t		int32;
+//! 64 bit signed integer.
+typedef int64_t		int64;
+
+//! 32 bit signed floating point number.
+typedef float		float32;
+//! 64 bit signed floating point number.
+typedef double		float64;
+
+//! 8 bit volatile unsigned integer.
+typedef volatile uint8_t	vuint8;
+//! 16 bit volatile unsigned integer.
+typedef volatile uint16_t	vuint16;
+//! 32 bit volatile unsigned integer.
+typedef volatile uint32_t	vuint32;
+//! 64 bit volatile unsigned integer.
+typedef volatile uint64_t	vuint64;
+
+//! 8 bit volatile signed integer.
+typedef volatile int8_t		vint8;
+//! 16 bit volatile signed integer.
+typedef volatile int16_t	vint16;
+//! 32 bit volatile signed integer.
+typedef volatile int32_t	vint32;
+//! 64 bit volatile signed integer.
+typedef volatile int64_t	vint64;
+
+//! 32 bit volatile signed floating point number.
+typedef volatile float32        vfloat32;
+//! 64 bit volatile signed floating point number.
+typedef volatile float64        vfloat64;
+
+//! 8 bit unsigned integer.
+typedef uint8_t		byte;
+
+//! 8 bit unsigned integer.
+typedef uint8_t		u8;
+//! 16 bit unsigned integer.
+typedef uint16_t	u16;
+//! 32 bit unsigned integer.
+typedef uint32_t	u32;
+//! 64 bit unsigned integer.
+typedef uint64_t	u64;
+
+//! 8 bit signed integer.
+typedef int8_t		s8;
+//! 16 bit signed integer.
+typedef int16_t		s16;
+//! 32 bit signed integer.
+typedef int32_t		s32;
+//! 64 bit signed integer.
+typedef int64_t		s64;
+
+//! 8 bit volatile unsigned integer.
+typedef volatile u8          vu8;
+//! 16 bit volatile unsigned integer.
+typedef volatile u16         vu16;
+//! 32 bit volatile unsigned integer.
+typedef volatile u32         vu32;
+//! 64 bit volatile unsigned integer.
+typedef volatile u64         vu64;
+
+//! 8 bit volatile signed integer.
+typedef volatile s8           vs8;
+//! 16 bit volatile signed integer.
+typedef volatile s16          vs16;
+//! 32 bit volatile signed integer.
+typedef volatile s32          vs32;
+//! 64 bit volatile signed integer.
+typedef volatile s64          vs64;
+
+using namespace std;
+
+#include "SDL.h"
+
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
